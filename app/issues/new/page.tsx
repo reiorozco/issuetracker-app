9d7eba00427701ae8187effcf2c9d47e76449e44
reiorozco <rfoc15@gmail.com>
2025-02-06 12:11:32 -0500

    handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,12 +3,23 @@
 import React, { useMemo } from "react";
 import { Button, TextField } from "@radix-ui/themes";
 import { MdAdd } from "react-icons/md";
+import { Controller, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 import SimpleMDE from "react-simplemde-editor";
 import EasyMDE from "easymde";
+import axios from "axios";
 
 import "easymde/dist/easymde.min.css";
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 function NewIssuePage() {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   const SimpleMDEOptions = useMemo(() => {
     return {
       // spellChecker: false,
@@ -17,17 +28,35 @@ function NewIssuePage() {
   }, []);
 
   return (
-    <div className="max-w-xl space-y-2">
-      <TextField.Root placeholder="Title...">
+    <form
+      className="max-w-xl space-y-2"
+      onSubmit={handleSubmit(async (data) => {
+        console.log("IssueForm: ", data);
+
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root placeholder="Title..." {...register("title")}>
         <TextField.Slot>
           <MdAdd height="16" width="16" />
         </TextField.Slot>
       </TextField.Root>
 
-      <SimpleMDE placeholder="Description..." options={SimpleMDEOptions} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE
+            placeholder="Description..."
+            options={SimpleMDEOptions}
+            {...field}
+          />
+        )}
+      />
 
-      <Button>Submit New Issue</Button>
-    </div>
+      <Button type="submit">Submit New Issue</Button>
+    </form>
   );
 }
 
