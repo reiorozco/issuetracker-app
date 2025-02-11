11c8abcf3ebae5af883d83c0f6030c5cf68a977b
reiorozco <rfoc15@gmail.com>
2025-02-11 07:25:10 -0500

    Add a confirm dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,13 +1,42 @@
 import React from "react";
 import { MdDeleteOutline } from "react-icons/md";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 function DeleteIssueButton({ issueId }: { issueId: number }) {
   return (
-    <Button color="tomato">
-      <MdDeleteOutline />
-      Edit Issue
-    </Button>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="tomato">
+            <MdDeleteOutline />
+            Edit Issue
+          </Button>
+        </AlertDialog.Trigger>
+
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Confirm Delete</AlertDialog.Title>
+
+          <AlertDialog.Description size="2">
+            Are you sure you want to delete this issue? This action cannot be
+            undone.
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+
+            <AlertDialog.Action>
+              <Button variant="solid" color="tomato">
+                Delete
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 }
 
