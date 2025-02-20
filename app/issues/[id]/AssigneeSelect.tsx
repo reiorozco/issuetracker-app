25cfa7ef55ca3e0add576ea5eb515099c1cc3660
reiorozco <rfoc15@gmail.com>
2025-02-20 09:14:19 -0500

    Populate the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Select } from "@radix-ui/themes";
+import { User } from "@prisma/client";
 
 function AssigneeSelect() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+
+      setUsers(data);
+    };
+
+    fetchUsers().then(() => console.log("Fetch users ok"));
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
@@ -10,7 +26,12 @@ function AssigneeSelect() {
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
 
-          <Select.Item value="1">Rei Orozco</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.email} value="1">
+              {user.name}
+            </Select.Item>
+          ))}
+
           <Select.Item value="2">John Doe</Select.Item>
           <Select.Item value="3">Agus Clavijo</Select.Item>
         </Select.Group>
